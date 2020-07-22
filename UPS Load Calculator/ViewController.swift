20c504a71df894d4856637d98627d6fcf7032dc6
deepakreddy <deepak.jonnalagadda@gmail.com>
2020-07-22 10:46:32 +0530

    Initial Commit

---
+++ UPS Load Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UPS Load Calculator
+//
+//  Created by Deepak Reddy on 22/07/20.
+//  Copyright © 2020 Deepak Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
